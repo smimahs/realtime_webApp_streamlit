df3500acfc9df801a6a3ca60118aafa9a785f9cf
mimahs <shamims12@yahoo.com>
2020-06-17 14:04:34 +0430

    add dataset

---
+++ MyApp.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import plot_confusion_matrix, plot_roc_curve, plot_precision_recall_curve
+from sklearn.metrics import precision_score, recall_score
+
+
+
+
+def main():
+    
+
+
+
+if __name__ == '__main__':
+    main()
